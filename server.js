f82ea899c84b8d500e400a2ad1670a217b8889e0
Pat Downer <pat.downer85@gmail.com>
2021-06-22 11:57:31 -0700

    gutted react app

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express')
-
+const { join } = require('path')
 const app = express()
+
+// static middleware for Heroku
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(join(__dirname, 'client', 'build')))
+}
 
 // middleware
 app.use(express.urlencoded({ extended: true }))
@@ -8,6 +13,13 @@ app.use(express.json())
 
 // add routes folder
 app.use(require('./routes'))
+
+// for Heroku, catch all route
+if (process.env.NODE_ENV === 'production') {
+  app.get('/*', (req, res) => {
+    res.sendFile(join(__dirname, 'client', 'build', 'index.html'))
+  })
+}
 
 // bring in db connection
 require('./db')
